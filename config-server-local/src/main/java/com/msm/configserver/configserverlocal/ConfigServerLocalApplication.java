4ecbdcd5b47b6bbfed019bd1989345424ee4b144
Madushanka Srimal Muttettuwa <msmuttettuwa@Virtusa.com>
2021-03-08 12:48:43 +0530

    initial commit

---
+++ config-server-local/src/main/java/com/msm/configserver/configserverlocal/ConfigServerLocalApplication.java
@@ -0,0 +1,15 @@
+package com.msm.configserver.configserverlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerLocalApplication.class, args);
+	}
+
+}
---
+++ config-server-local/src/test/java/com/msm/configserver/configserverlocal/ConfigServerLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msm.configserver.configserverlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
